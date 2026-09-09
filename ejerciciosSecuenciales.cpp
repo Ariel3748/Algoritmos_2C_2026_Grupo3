46e8237e147c3829651b0aa2d8640140b7efe592
LisandroDev <lisandrorubi@gmail.com>
2026-09-09 18:15:16 -0300

    agrega ejercicios a los archivos existentes

--- ejerciciosSecuenciales.cpp
+++ ejerciciosSecuenciales.cpp
@@ -383,6 +383,47 @@ int main(int argc, char const *argv[])
 
     //Ejercicio 21
     
+//  int main() {
+//   bool esDescendente = true;
+//   bool esAscendente = true;
+
+//   float numeroPrevio = 0;
+//   float actual;
+
+//   while (true) {
+//     cout << "Ingrese numero (0 para terminar la serie): " << endl;
+//     cin >> actual;
+
+//     if (actual == 0) {
+//       break;
+//     }
+
+//     if (numeroPrevio == 0) {
+//       numeroPrevio = actual;
+//       continue;
+//     }
+
+//     if (numeroPrevio > actual) {
+//       esAscendente = false;
+//     }
+
+//     if (numeroPrevio < actual) {
+//       esDescendente = false;
+//     }
+
+//     numeroPrevio = actual;
+//   }
+
+//   if (esAscendente) {
+//     cout << "La serie es ascendente" << endl;
+//   } else if (esDescendente) {
+//     cout << "La serie es descendente" << endl;
+//   } else {
+//     cout << "La serie no tiene orden" << endl;
+//   }
+
+//   return 0;
+// }
 
 
 
